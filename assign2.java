fb908e27c8b4634f5d4b354a8369206120319e3a
SoummoSsj <81679223+SoummoSsj@users.noreply.github.com>
2023-03-10 12:50:59 +0600

    assign 2 added to sec branch

---
+++ assign2.java
@@ -0,0 +1,41 @@
+package com.example.dummy;
+
+public class assign2 {
+   private String name;
+   private int age;
+    private String mail;
+
+    public assign2(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public assign2(String name, String mail) {
+        this.name = name;
+        this.mail = mail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
+
