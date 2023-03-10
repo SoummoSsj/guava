7add6d59995adda95b3c2356c3793fdf6d622d56
SoummoSsj <81679223+SoummoSsj@users.noreply.github.com>
2023-03-10 12:04:39 +0600

    assign.java added

---
+++ assign.java
@@ -0,0 +1,28 @@
+package com.example.dummy;
+
+public class assign {
+   private String name;
+    private String mail;
+
+    public assign(String name, String mail) {
+        this.name = name;
+        this.mail = mail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
+
